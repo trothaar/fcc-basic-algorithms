5f8aaa5bc14ded0ba50a2cbd389f6b6cca7b40c2
Teresa <trothaar@users.noreply.github.com>
2017-02-17 11:02:30 -0500

    Add files via upload

---
+++ missing-letters.js
@@ -0,0 +1,10 @@
+/* Teresa Rothaar
+   Free Code Camp Challenge #271 - Missing Letters
+
+   Find the missing letter in the passed letter range and return it.
+   If all letters are present in the range, return undefined.
+
+   For example, fearNotLetter("abce") should return "d",
+   and fearNotLetter("bcd") should return undefined.
+
+*/
